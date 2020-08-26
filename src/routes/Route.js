e07567fa1ea91183f04edaf9747e6107b2396d51
Douglas Lima <douglasproglima@gmail.com>
2020-08-25 22:05:25 -0300

    Layouts das páginas

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,20 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
-  ...restOperator
+  ...restProperties
 }) {
-  const signed = false;
+  const signed = true;
 
   if (!signed && isPrivate) return <Redirect to="/" />;
   if (signed && !isPrivate) return <Redirect to="/dashboard" />;
 
-  return <Route {...restOperator} component={Component} />;
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...restProperties}
+      render={(props) => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
-RouteWrapper.protoTypes = {
+RouteWrapper.propTypes = {
   isPrivate: PropTypes.bool,
   component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
     .isRequired,
