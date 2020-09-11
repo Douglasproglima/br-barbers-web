68da8648219024bc13edfb8a90a6b291940137de
Douglas Lima <douglasproglima@gmail.com>
2020-09-11 12:46:28 -0300

    Add Toastify no projeto

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer, Flip } from 'react-toastify';
 import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
@@ -15,6 +16,19 @@ function App() {
         <Router history={history}>
           <Routes />
           <GlobalStyle />
+          <ToastContainer
+          className="toast-container"
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnHover={false}
+          draggable={true}
+          progress={undefined}
+          transition={Flip}
+        />
         </Router>
       </PersistGate>
     </Provider>
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,5 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
-
+import { toast } from 'react-toastify';
 import api from '~/services/api';
 import history from '~/services/history';
 import { signInSuccess, signFailure } from './actions';
@@ -15,7 +15,7 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.log('usuario nao é prestador de servico');
+      toast.error('🦄 Usuário não é um prestador de serviço.');
       return;
     }
 
@@ -23,6 +23,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (error) {
+    toast.error('🦄 Falha na autenticação, verifique seus dados.');
     yield put(signFailure());
   }
 }
