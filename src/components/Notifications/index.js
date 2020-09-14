210b91422094eba58d25796538576a2475471bde
Douglas Lima <douglasproglima@gmail.com>
2020-09-13 21:56:31 -0300

    Componente Notificação + Estialização

---
+++ src/components/Notifications/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { MdNotifications } from 'react-icons/md';
+import {
+  Container,
+  Badge,
+  NotificationList,
+  Notification,
+  Scroll,
+} from './styles';
+
+export default function Notifications() {
+  return (
+    <Container>
+      <Badge hasUnread>
+        <MdNotifications color="#f47b00" size={20} />
+      </Badge>
+
+      <NotificationList>
+        <Scroll>
+          <Notification unread>
+            <p>Você possui um novo agendamento. Verifique!</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+
+          <Notification>
+            <p>Você possui um novo agendamento. Verifique!</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+
+          <Notification unread>
+            <p>Você possui um novo agendamento. Verifique!</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+
+          <Notification unread>
+            <p>Você possui um novo agendamento. Verifique!</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+
+          <Notification>
+            <p>Você possui um novo agendamento. Verifique!</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+
+          <Notification unread>
+            <p>Você possui um novo agendamento. Verifique!</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+
+          <Notification unread>
+            <p>Você possui um novo agendamento. Verifique!</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+
+          <Notification>
+            <p>Você possui um novo agendamento. Verifique!</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+
+          <Notification unread>
+            <p>Você possui um novo agendamento. Verifique!</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+        </Scroll>
+      </NotificationList>
+    </Container>
+  );
+}
