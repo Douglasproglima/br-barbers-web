2294260cc716decfecef671162c137a2e4c05fae
Douglas Lima <douglasproglima@gmail.com>
2020-09-22 20:07:47 -0300

    Feature Logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import logo from '~/assets/logo_min.svg';
 import { Container, Content, Profile } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
 import Notifications from '~/components/Notifications';
 
 export default function Header() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
 
   return (
     <Container>
@@ -23,14 +29,16 @@ export default function Header() {
             <div>
               <strong>{profile.name}</strong>
               <Link to="/profile">Perfil</Link>
-              <Link to="/">Sair</Link>
+              <Link to="/" onClick={handleSignOut}>
+                Sair
+              </Link>
             </div>
             <img
               src={
                 profile.avatar.url ||
                 'https://api.adorable.io/avatars/50/abott@adorable.png'
               }
-              alt="Douglas Lima"
+              alt={profile.name}
             />
           </Profile>
         </aside>
